2b97452052af98539934e17b324342361ff26093
andreock <vivanco11.ac@gmail.com>
2023-12-28 23:41:06 +0100

    Make capibaraZero config works with xtensa toolchain

--- shell_configs/capibara_zero.nix
+++ shell_configs/capibara_zero.nix
@@ -2,19 +2,20 @@ let pkgs = import <nixpkgs> {};
 
 in pkgs.mkShell rec {
   name = "platformio_development";
-  
   buildInputs = with pkgs; [
     # Platformio deps
-    platformio 
-    clang 
-    gcc 
-    python3
+    platformio
     git
+    python3
     # Documentation deps
     nodejs
     fritzing
     # Neovim deps
-    # neovim
     vscode
   ];
+  shellHook = ''
+    # fixes libstdc++ issues and libgl.so issues
+    LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib64/
+    patchelf --set-interpreter ${pkgs.stdenv.cc.bintools.dynamicLinker} ~/.platformio/packages/toolchain-xtensa-esp32s3/bin/xtensa-esp32s3-elf-gcc
+    '';
 }    
