851de32ce2bd312b0730ceca1e3f1a9078cb46ed
andreock <vivanco11.ac@gmail.com>
2023-12-28 23:41:35 +0100

    Add pip to python3 env

--- shell_configs/templates/python3.nix
+++ shell_configs/templates/python3.nix
@@ -3,13 +3,16 @@ in
 let
   my-python-packages = ps: with ps; [
     setuptools
+    pip
     # other python packages
   ];
 in pkgs.mkShell rec {
-  name = "nodejs_development";
+  name = "Python_development";
   LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";  
   buildInputs = with pkgs; [
     python3
+    python3Packages.pip
+    python3Packages.pylint
     git
   ];
 }    
